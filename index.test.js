2ef4b1543f71520d97d00d9e8e525a8b1bfd7593
Qqkyu <kulmisiu007@gmail.com>
2023-12-13 21:33:44 +0100

    Add tests for files outside project

--- index.test.js
+++ index.test.js
@@ -1,23 +1,39 @@
 const { isNodeModulesImport } = require(".");
 
 describe("isNodeModulesImport", () => {
-  test("returns false for relative non node_modules import path", () => {
-    expect(isNodeModulesImport(".", "./index.js")).toBe(false);
+  describe("for a file inside project", () => {
+    test("returns false for relative non node_modules import path", () => {
+      expect(isNodeModulesImport(".", "./index.js")).toBe(false);
+    });
+
+    test("returns false for absolute non node_modules import path", () => {
+      expect(isNodeModulesImport(".", "./index.js")).toBe(false);
+    });
+
+    test("returns true for one-level node_modules import path", () => {
+      expect(isNodeModulesImport(".", "prettier")).toBe(true);
+    });
+
+    test("returns true for two-level node_modules import path", () => {
+      expect(isNodeModulesImport(".", "prettier/plugins")).toBe(true);
+    });
+
+    test("returns true for relative node_modules import path", () => {
+      expect(isNodeModulesImport(".", "./node_modules/prettier")).toBe(true);
+    });
   });
 
-  test("returns false for absolute non node_modules import path", () => {
-    expect(isNodeModulesImport(".", "./index.js")).toBe(false);
-  });
+  describe("for a file outside project", () => {
+    test("returns false for project one-level node_modules import path", () => {
+      expect(isNodeModulesImport("../index.js", "prettier")).toBe(false);
+    });
 
-  test("returns true for one-level node_modules import path", () => {
-    expect(isNodeModulesImport(".", "prettier")).toBe(true);
-  });
+    test("returns false for project two-level node_modules import path", () => {
+      expect(isNodeModulesImport("../index.js", "prettier/plugins")).toBe(false);
+    });
 
-  test("returns true for two-level node_modules import path", () => {
-    expect(isNodeModulesImport(".", "prettier/plugins")).toBe(true);
-  });
-
-  test("returns true for relative node_modules import path", () => {
-    expect(isNodeModulesImport(".", "./node_modules/prettier")).toBe(true);
+    test("returns false for project relative node_modules import path", () => {
+      expect(isNodeModulesImport("../index.js", "./node_modules/prettier")).toBe(false);
+    });
   });
 });
