637a4da2173866b598bd6d27b491d7f2da4eb438
Qqkyu <kulmisiu007@gmail.com>
2023-12-14 21:32:55 +0100

    Add tests for non-existent node_modules imports

--- index.test.js
+++ index.test.js
@@ -14,6 +14,10 @@ describe("isNodeModulesImport", () => {
       expect(isNodeModulesImport(".", "prettier")).toBe(true);
     });
 
+    test("returns false for one-level non-existent node_modules import path", () => {
+      expect(isNodeModulesImport(".", "react")).toBe(false);
+    });
+
     test("returns true for two-level node_modules import path", () => {
       expect(isNodeModulesImport(".", "prettier/plugins")).toBe(true);
     });
@@ -42,6 +46,10 @@ describe("isNodeModulesImport", () => {
 
     test("returns false for project relative node_modules import path", () => {
       expect(isNodeModulesImport("../index.js", "./node_modules/prettier")).toBe(false);
+    });
+
+    test("returns false for project absolute non-existent node_modules import path", () => {
+      expect(isNodeModulesImport("../index.js", `${__dirname}/node_modules/react`)).toBe(false);
     });
 
     test("returns true for relative node_modules import path", () => {
